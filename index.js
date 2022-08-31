f33354a321c41a9ea1f2cef18062b514a9dcd64e
Mohamed Gamal <MohamedGamal.Dv@gmail.com>
2022-08-31 23:58:31 +0200

    Add Elements Reference

---
+++ index.js
@@ -0,0 +1,8 @@
+// Ref's
+let app = document.querySelector('.app');
+let formWrap = document.querySelector('.task-form');
+let form = document.querySelector('.task-form-add');
+let input = document.querySelector('.task-input');
+let submit = document.querySelector('.task-button');
+let listWrap = document.querySelector('.list-wrap');
+

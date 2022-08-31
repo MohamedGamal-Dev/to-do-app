d4611afa9bc8af012720e668a4a0430ae8223eac
Mohamed Gamal <MohamedGamal.Dv@gmail.com>
2022-08-31 23:59:39 +0200

    Add Form & List Initial Setup

--- index.js
+++ index.js
@@ -6,3 +6,16 @@ let input = document.querySelector('.task-input');
 let submit = document.querySelector('.task-button');
 let listWrap = document.querySelector('.list-wrap');
 
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  console.log(input.value);
+  addData();
+});
+
+let data = [];
+
+let addData = () => {
+  // {id:number, task:string, completed:boolean}
+  data.push({ task: input.value });
+  console.log(data);
+};

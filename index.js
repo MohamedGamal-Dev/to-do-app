d1662e8034dc5fb19cf5dd4676fde8b12dba0289
Mohamed Gamal <MohamedGamal.Dv@gmail.com>
2022-09-04 21:54:48 +0200

    Add Initial render tasks list

--- index.js
+++ index.js
@@ -5,17 +5,32 @@ let form = document.querySelector('.task-form-add');
 let input = document.querySelector('.task-input');
 let submit = document.querySelector('.task-button');
 let listWrap = document.querySelector('.list-wrap');
+let tasks = document.querySelector('.tasks-list');
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
-  console.log(input.value);
-  addData();
+
+  createData();
 });
 
 let data = [];
 
-let addData = () => {
+let createData = () => {
   // {id:number, task:string, completed:boolean}
-  data.push({ task: input.value });
+  let newTask = { id: null, task: input.value, completed: false };
+  data.push(newTask);
+  renderTask();
   console.log(data);
+};
+
+let renderTask = () => {
+  tasks.innerHTML = '';
+  data.map((taskItem) => {
+    tasks.innerHTML += `
+    <div>
+    <span>${taskItem.task}</span>
+    </div>
+    `;
+    input.value = '';
+  });
 };
